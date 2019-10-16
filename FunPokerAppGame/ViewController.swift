b003b9697975964809763ac70987d0675013c796
John Campbell <johncampbell@Johns-MacBook-Pro-6.local>
2019-10-16 11:58:40 +0100

    Initial Commit

---
+++ FunPokerAppGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunPokerAppGame
+//
+//  Created by John Campbell on 16/10/2019.
+//  Copyright © 2019 John Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FunPokerAppGameTests/FunPokerAppGameTests.swift
@@ -0,0 +1,34 @@
+//
+//  FunPokerAppGameTests.swift
+//  FunPokerAppGameTests
+//
+//  Created by John Campbell on 16/10/2019.
+//  Copyright © 2019 John Campbell. All rights reserved.
+//
+
+import XCTest
+@testable import FunPokerAppGame
+
+class FunPokerAppGameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
